f01685063dcf5ffc3eb17488e537b21fb688b3d3
danrie19 <nataliriera19@gmail.com>
2022-03-23 13:31:26 -0400

    landing page change

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,25 @@
 import "../App.css";
+import React from "react";
+import landing from "../assets/landing.jpg";
 
 function HomePage() {
  
   return (
     <div className="App">
-     <div>HOMEPAGE</div>
-     
+
+<header className="landingpage">
+
+  <div>
+  <h1>Welcome to a space for tech engineers!</h1>
+  <p>With Tech & Tapas you can sign up, edit your portfolio site, make blog posts and explore other users posts and portfolios while staying up to date on the latest tech news and tech meet ups.</p>
+  
+  <a href="/auth/signup"><button >Get Started</button></a>
+  
+  </div>
+
+  <img src={landing}/>   
+
+  </header>  
     </div>
   );
 }
