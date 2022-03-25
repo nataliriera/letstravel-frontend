2bf6bc46c94be3f56c49a7c3d7ed065ca528e383
nataliriera <67808802+nataliriera@users.noreply.github.com>
2022-03-25 18:19:58 -0400

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 //valida si la aplicion esta en produccion nos retorna un true or false
 const isProduction = process.env.NODE_ENV === "production";
 const baseURL = isProduction
-  ? " https://techandtapas.herokuapp.com/"
+  ? " https://techandtapas.herokuapp.com/api"
   : "http://localhost:5005/api";
 
 export const api = axios.create({
