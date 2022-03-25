6bf603e44a8a8915dac6969d5b4af11dd714f835
Natali Riera <nataliriera19@gmail.com>
2022-03-25 00:19:08 -0400

    backend url inserted

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 //valida si la aplicion esta en produccion nos retorna un true or false
 const isProduction = process.env.NODE_ENV === "production";
-const baseURL = isProduction ? "aqui va URL de backend" : "http://localhost:5005/api";
+const baseURL = isProduction ? " https://techandtapas.herokuapp.com/" : "http://localhost:5005/api";
 
 export const api = axios.create({
     //baseURL : baseURL
