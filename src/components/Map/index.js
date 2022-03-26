790df793163b42cf607e997e357675608f2b1f9c
Natali Riera <nataliriera19@gmail.com>
2022-03-25 20:37:20 -0400

    work loader

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -3,7 +3,13 @@ import Map, { Marker, Popup } from "react-map-gl";
 import { getpin } from "../../services/pinService";
 import { Room } from "@material-ui/icons";
 import "./map.css";
+import mapboxgl from 'mapbox-gl';
 
+// The following is required to stop "npm build" from transpiling mapbox code.
+// notice the exclamation point in the import.
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 function PinMap() {
   const [viewport, setViewport] = useState({
     latitude: 47.040182,
